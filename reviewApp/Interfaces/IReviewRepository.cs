cde153b7a5601f6fa400b3ab6149246ea24d2e81
stefanAntonic <stefandantonic@gmail.com>
2023-03-26 23:24:53 +0200

    [002] created ReviewController

---
+++ reviewApp/Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+using reviewApp.Models;
+
+namespace reviewApp.Interfaces;
+
+public interface IReviewRepository
+{
+    ICollection<Review> GetReviews();
+    Review GetReview(int reviewId);
+    ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+    bool ReviewExists(int reviewId);
+
+}
